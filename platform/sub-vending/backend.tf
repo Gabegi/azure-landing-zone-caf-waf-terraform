3425af953a5cc21ecbaab021ecdb86478fd4da91
You have one new message <george.trombley6000@gmail.com>
2026-04-30 15:51:38 +0200

    backend

--- platform/sub-vending/backend.tf
+++ platform/sub-vending/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "remote-state"
     storage_account_name = "therealbestnamesa"
-    container_name       = "lz-caf-waf-rfstate"
+    container_name       = "platform-sub-vending-tfstate"
     key                  = "platform/sub-vending.tfstate"
   }
 }
