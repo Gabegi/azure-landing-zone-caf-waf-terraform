6e78c55f3aa7560dba3e45da0e4f7ed99f720dde
You have one new message <george.trombley6000@gmail.com>
2026-05-05 10:26:09 +0200

    Update variables.tf

--- modules/naming/variables.tf
+++ modules/naming/variables.tf
@@ -1,11 +1,16 @@
 variable "prefix" {
-  description = "List of prefixes prepended to resource names (e.g. [\"contoso\"])."
+  description = "Org prefix prepended to all resource names. Defaults to the Contoso abbreviation."
   type        = list(string)
-  default     = []
+  default     = ["conto"]
+
+  validation {
+    condition     = length(var.prefix) > 0
+    error_message = "prefix must contain at least the org abbreviation."
+  }
 }
 
 variable "suffix" {
-  description = "List of suffixes appended to resource names (e.g. [\"prod\"])."
+  description = "List of suffixes appended to resource names, typically env + region (e.g. [\"prod\", \"eus\"])."
   type        = list(string)
   default     = []
 }
